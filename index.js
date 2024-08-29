3fb8b69a97315e426626bebcfd2451b235fc5661
Yuvraj <devyuvraj883@gmail.com>
2024-08-30 04:53:22 +0530

    Fixed module not found

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const urlRoute = require('./router/url');
 
-const connectToMongoose = require('./controller');
+const connectToMongoose = require('./connection');
 
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 const url = 'mongodb+srv://kingyuvi88:<db_password>@cluster0.n6zna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 connectToMongoose(url).then(()=>{console.log("MongoDB connected")});
 app.use('/url', urlRoute);
