885bbe507b3ba892d23064d8125fa5d3358c21bc
Yuvraj <devyuvraj883@gmail.com>
2024-08-30 04:28:14 +0530

    Created handleGenerateShortURL controller

---
+++ controller/url.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const shortID = require('shortid');
+
+const URL = require('../models/url');
+
+const handleGenerateShortURL = async (req, res)=>{
+  const body = req.body;
+  const s_ID = shortID.generate();
+  if(!body.url){
+    return res.status(400).json({error:'URL is required'});
+  }
+  await URL.create({
+    shortId: s_ID,
+    redirectURL: body.url,
+    visitHistory:[]
+
+  })
+  return res.json({id: s_ID});
+}
+
+module.export = {
+  handleGenerateShortURL
+}
