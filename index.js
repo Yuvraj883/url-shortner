72e421a18cc12ee5aa3eedce573335c2f925246d
Yuvraj <devyuvraj883@gmail.com>
2024-08-30 15:42:28 +0530

    Added middleware to parse json

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const urlRoute = require('./router/url');
 
 const connectToMongoose = require('./connection');
+// const connectDB = require('./connection')
 
 const app = express();
+
 const PORT = 8001;
-const url = 'mongodb+srv://kingyuvi88:<db_password>@cluster0.n6zna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const url = "mongodb+srv://kingyuvi88:TheGodYuvi@cluster0.n6zna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 connectToMongoose(url).then(()=>{console.log("MongoDB connected")});
+
+app.use(express.json()); //middleware to parse json
 app.use('/url', urlRoute);
 app.listen(PORT, ()=>{console.log(`Server running at port:${PORT}`)});
